ffd8e169833069e5995bbeca08d49607a84d9f6e
kaju111 <saikiakaju275@gmail.com>
2023-05-27 20:59:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,98 @@
-import axios from "axios";
-import "./App.css";
-import Results from "./Components/Results";
-import axois from "axios";
+import Search from "./Components/Search";
+import Result from "./Components/Result";
 import { useEffect, useState } from "react";
-const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query="
-
+import axios from "axios";
 function App() {
-  const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState("");
-
-
-  const changeTheSearch = (event) => {
-
-    setSearch(event.target.value)
-
-
-
+  const [weather, setWeather] = useState([]);
+  const [history, setHistory] = useState([]);
+  const changeSearch = (value) => {
+    setSearch(value);
   }
 
+  // const getWeatherData = () => {
+  //   axios.get(
+  //     `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=3265874a2c77ae4a04bb96236a642d2f&units=metric`
+  //   )
+  //     .then(
+  //       (response) => {
+  //         console.log(response);
+  //       }
+  //     ).catch(
+  //       (error) => {
+  //         console.log(error);
+  //       }
+  //     )
 
+  // }
 
-  const getAllMovies = ( ) => {
-    axios.get(APIURL)
-    .then(
-      (response) => {
-        setMovies(response.data.results)          
-      }
-    )
-      .catch(
-        (error) => {
-          console.log(error)
-        }
+  const searchWeatherHandler = () => {
+
+    if (search !== "") {
+      axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=3265874a2c77ae4a04bb96236a642d2f&units=metric`
       )
+        .then(
+          (response) => {
+            if (history.indexOf(search) === -1) {
+              setHistory(
+                [
+                  ...history,
+                  search
+                ]
+              )
+            }
+            // console.log(response.data);
+            setWeather(response.data);
+          }
+        ).catch(
+          (error) => {
+            console.log(error);
+          }
+        )
+    }
   }
 
-const getSearchedMovies = () => {
-    axois.get(
-      SEARCHAPI+search 
-
-    )
-    .then(
-      (response) => {
-        console.log(response.data.results)
-        setMovies(response.data.results)
-      }
-    )
-   .catch(
-    (error) => {
-      console.log(error)
+  const historySearchHandler = async (data) => {
+    setSearch(data)
+    if (data !== "") {
+      axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=3265874a2c77ae4a04bb96236a642d2f&units=metric`
+      )
+        .then(
+          (response) => {
+            if (history.indexOf(data) === -1) {
+              setHistory(
+                [
+                  ...history,
+                  data
+                ]
+              )
+            }
+            // console.log(response.data);
+            setWeather(response.data);
+          }
+        ).catch(
+          (error) => {
+            console.log(error);
+          }
+        )
     }
-   )
+  }
 
-}
-
-  useEffect(
-    () => {
-      // console.log("hello")
-      if(search === "") {
-        getAllMovies()
-      }
-      else{
-        getSearchedMovies();
-      }
-    },
-    [search]
-  )
+  // useEffect(
+  //   () => {
+  //     if(search !== ""){
+  //        getWeatherData();
+  //     }
+  //   },
+  //   [search]
+  // )
 
   return (
-    <div className="max-w-[1240px] shadow-xl min-h-[400px] mx-auto p-3">
-      <input
-        value={search}
-        onChange={changeTheSearch}
-        type="search"
-        className="w-full border border-black rounded text-slate-700 p-4"
-      />
-      <Results movies={movies} />
+    <div className="max-w-[1240px] mx-auto mt-2 p-3">
+      <Search searchData={search} eventHandler={changeSearch} searchWeather={searchWeatherHandler} />
+      <Result weatherData={weather} historyData={history} historySearch={historySearchHandler} />
     </div>
   );
 }
